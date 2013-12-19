bca5622b721ecbf223f19f41607b9c4a7523f347
Curryst <scurry@datalliance.com>
2013-12-19 16:14:12 -0500

    Eclipse setup.

---
+++ src/main/java/io/krypto/darknet/DarkNet.java
@@ -0,0 +1,16 @@
+package io.krypto.darknet;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DarkNet extends JavaPlugin {
+
+	@Override
+	public void onEnable(){
+		// TODO
+	}
+
+	@Override
+	public void onDisable(){
+		// TODO
+	}
+}
